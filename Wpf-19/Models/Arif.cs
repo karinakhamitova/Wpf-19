e70ec54c1ecaeadf3fb1281b6231bcae61798a77
TopFps <TopFps@DESKTOP-UUEU6H0>
2023-01-05 23:44:54 +0300

    Добавьте файлы проекта.

---
+++ Wpf-19/Models/Arif.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_19.Models
+{
+    static class Arif
+    {
+        public static double Lenght(double a) => a*2*Math.PI;
+    }
+}
---
+++ Wpf-19/ViewModels/MyWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Wpf_19.Models;
+
+namespace Wpf_19.ViewModels
+{
+    class MyWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double number1;
+
+        public double Number1
+        {
+            get { return number1; }
+            set
+            {
+                number1 = value;
+                OnPropertyChanged();
+            }
+        }
+        private double number2;
+
+        public double Number2
+        {
+            get { return number2; }
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+      
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p) 
+        {
+            Number2 = Arif.Lenght(Number1);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (Number1!=0)
+                return true;
+            else
+                return false;
+        }
+        public MyWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
